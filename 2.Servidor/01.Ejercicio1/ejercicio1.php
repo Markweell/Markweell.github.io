d560c1e2ef993cad9917bb020790e5dbba5fe5cb
Markweell <marcosgallardoperez@gmail.com>
2018-10-10 10:22:38 +0200

    Primera subida

---
+++ 2.Servidor/01.Ejercicio1/ejercicio1.php
@@ -0,0 +1,12 @@
+<?php
+$n1=90;
+echo "modEl primer numero es $n1 <br> ";
+$n2=78;
+echo "El segundo numero es $n2 <br> ";
+if($n1>$n2)
+echo "El mayor de ellos es $n1";
+elseif($n1==$n2)
+echo "Los numeros son iguales";
+else
+echo "El mayor de ellos es $n2";
+?>
