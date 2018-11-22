e6c536224bcb2ff496672412ca2a2ce736c94ef2
Markweell <marcosgallardoperez@gmail.com>
2018-11-22 20:12:46 +0100

    Refactorizando algunas variables mal escritas.

--- 1.Cliente/Tema3/PropuestaDeExamen/js/solucion.js
+++ 1.Cliente/Tema3/PropuestaDeExamen/js/solucion.js
@@ -6,17 +6,17 @@
     {
         let regexNombre;
         var usuarios;
-        let DOMNombreUsuario;
-        let DOMContaseniaUsuario;
-        let DOMEstado;
+        let domNombreUsuario;
+        let domContaseniaUsuario;
+        let domEstado;
 
         /**
          * Funcion inicial al cargar el DOM
          */
         function init() {
-            DOMNombreUsuario = document.getElementById('nombreUsuario');
-            DOMContaseniaUsuario = document.getElementById('contraseniaUsuario');
-            DOMEstado = document.getElementById('estado');
+            domNombreUsuario = document.getElementById('nombreUsuario');
+            domContaseniaUsuario = document.getElementById('contraseniaUsuario');
+            domEstado = document.getElementById('estado');
             regexNombre = /^[a-z]{3,10}$/i;
             //if (leerClaveLocal() == null)
             usuarios = new Map();
@@ -35,8 +35,8 @@
          * Asigna el comportamiento al formulario
          */
         function asignarComportamientoALosInput() {
-            DOMNombreUsuario.addEventListener('blur', comprobarNombre);
-            DOMNombreUsuario.addEventListener('focus', letraNegra);
+            domNombreUsuario.addEventListener('blur', comprobarNombre);
+            domNombreUsuario.addEventListener('focus', letraNegra);
             document.getElementById('entar').addEventListener('click', entrar);
             document.getElementById('registarse').addEventListener('click', registarse);
             //document.getElementById('clearDataBase').addEventListener('click', clearDataBase);
@@ -45,15 +45,15 @@
          * Vuelve la letra el input del Nombre a su estado por defecto.
          */
         function letraNegra() {
-            DOMNombreUsuario.style.color = 'black';
-            DOMEstado.innerHTML = "";
+            domNombreUsuario.style.color = 'black';
+            domEstado.innerHTML = "";
         }
         /**
          * Comprueba que el nombre cumple el patrón, si no lo cumple, cambia su color a rojo
          */
         function comprobarNombre() {
-            if (!regexNombre.test(DOMNombreUsuario.value))
-                DOMNombreUsuario.style.color = 'red';
+            if (!regexNombre.test(domNombreUsuario.value))
+                domNombreUsuario.style.color = 'red';
                 
         }
         /**
@@ -65,9 +65,9 @@
             }
             try {
                 comprobarUsuarioExistente();
-                (usuarios.get(DOMNombreUsuario.value) == DOMContaseniaUsuario.value) ? DOMEstado.innerHTML = "Bienvenido" : DOMEstado.innerHTML = "Contaseña invalida";
+                (usuarios.get(domNombreUsuario.value) == domContaseniaUsuario.value) ? domEstado.innerHTML = "Bienvenido" : domEstado.innerHTML = "Contaseña invalida";
             } catch (e) {
-                DOMEstado.innerHTML = e.message;
+                domEstado.innerHTML = e.message;
             }
         }
         /**
@@ -79,18 +79,18 @@
             }
             try {
                 comprobarUsuarioExistente();
-                DOMEstado.innerHTML = "Se está intentando registar, estando ya en la base de datos";
+                domEstado.innerHTML = "Se está intentando registar, estando ya en la base de datos";
             } catch (e) {
-                usuarios.set(DOMNombreUsuario.value, DOMContaseniaUsuario.value);
+                usuarios.set(domNombreUsuario.value, domContaseniaUsuario.value);
                 //grabarClaveLocal(usuarios);
-                DOMEstado.innerHTML = "";
+                domEstado.innerHTML = "";
             }
         }
         /**
          * Comprueba si el usuario ya existe, en caso de que no exista lanza una excepción
          */
         function comprobarUsuarioExistente() {
-            if (!usuarios.has(DOMNombreUsuario.value))
+            if (!usuarios.has(domNombreUsuario.value))
                 throw new Error('Ese usuario no está!!');
             return true;
         }
@@ -98,9 +98,9 @@
          * Comprueba que el nombre de usuario o la contraseña no esten vacíos ni esten en rojo. 
          */
         function comprobacionInicial() {
-            if (DOMNombreUsuario.value == "" || DOMContaseniaUsuario.value == "" || DOMNombreUsuario.style.color == 'red'){
-                if(DOMNombreUsuario.style.color == 'red')
-                    DOMEstado.innerHTML = "El nombre no es válido";
+            if (domNombreUsuario.value == "" || domContaseniaUsuario.value == "" || domNombreUsuario.style.color == 'red'){
+                if(domNombreUsuario.style.color == 'red')
+                    domEstado.innerHTML = "El nombre no es válido";
                 return false;
             }
             return true;
