5b8f7206612ad15d6c9ba91b49f0bad971f98d1b
Markweell <marcosgallardoperez@gmail.com>
2018-11-22 20:03:56 +0100

    Documentando el examen close #5

--- 1.Cliente/Tema3/PropuestaDeExamen/js/solucion.js
+++ 1.Cliente/Tema3/PropuestaDeExamen/js/solucion.js
@@ -12,6 +12,9 @@
 
         document.addEventListener('DOMContentLoaded', init);
 
+        /**
+         * Funcion inicial al cargar el DOM
+         */
         function init() {
             DOMNombreUsuario = document.getElementById('nombreUsuario');
             DOMContaseniaUsuario = document.getElementById('contraseniaUsuario');
@@ -23,10 +26,16 @@
             //usuarios = leerClaveLocal();
             crearEsqueletoBasico();
         }
+        /**
+         * Crea el esqueleto básico de la página
+         */
         function crearEsqueletoBasico() {
             document.getElementsByTagName('h1')[0].innerHTML = "Marcos Gallardo Pérez. Propuesta de examen";
             asignarComportamientoALosInput();
         }
+        /**
+         * Asigna el comportamiento al formulario
+         */
         function asignarComportamientoALosInput() {
             DOMNombreUsuario.addEventListener('blur', comprobarNombre);
             DOMNombreUsuario.addEventListener('focus', letraNegra);
@@ -34,15 +43,24 @@
             document.getElementById('registarse').addEventListener('click', registarse);
             //document.getElementById('clearDataBase').addEventListener('click', clearDataBase);
         }
+        /**
+         * Vuelve la letra el input del Nombre a su estado por defecto.
+         */
         function letraNegra() {
             DOMNombreUsuario.style.color = 'black';
             DOMEstado.innerHTML = "";
         }
+        /**
+         * Comprueba que el nombre cumple el patrón, si no lo cumple, cambia su color a rojo
+         */
         function comprobarNombre() {
             if (!regexNombre.test(DOMNombreUsuario.value))
                 DOMNombreUsuario.style.color = 'red';
                 
         }
+        /**
+         * Comportamiento del botón entrar
+         */
         function entrar() {
             if (!comprobacionInicial()) {
                 return;
@@ -54,6 +72,9 @@
                 DOMEstado.innerHTML = e.message;
             }
         }
+        /**
+         * Comportamiento del botón registrarse
+         */
         function registarse() {
             if (!comprobacionInicial()) {
                 return;
@@ -67,11 +88,17 @@
                 DOMEstado.innerHTML = "";
             }
         }
+        /**
+         * Comprueba si el usuario ya existe, en caso de que no exista lanza una excepción
+         */
         function comprobarUsuarioExistente() {
             if (!usuarios.has(DOMNombreUsuario.value))
                 throw new Error('Ese usuario no está!!');
             return true;
         }
+        /**
+         * Comprueba que el nombre de usuario o la contraseña no esten vacíos ni esten en rojo. 
+         */
         function comprobacionInicial() {
             if (DOMNombreUsuario.value == "" || DOMContaseniaUsuario.value == "" || DOMNombreUsuario.style.color == 'red'){
                 if(DOMNombreUsuario.style.color == 'red')
