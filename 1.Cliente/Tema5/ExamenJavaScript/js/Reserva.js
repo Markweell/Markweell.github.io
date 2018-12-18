ef3ea21f30c5273c989b04088ec977fb77aaec60
Markweell <marcosgallardoperez@gmail.com>
2018-12-18 22:50:17 +0100

    Entrega Examen JavaScript

---
+++ 1.Cliente/Tema5/ExamenJavaScript/js/Reserva.js
@@ -0,0 +1,150 @@
+/**
+ * Clase Reserva
+ * @author Marcos Gallardo Pérez
+ */
+
+function Reserva(nombre = "", correo = "", fecha = "", hora = "", numNoches = "", numPersonas = "", servicioRes = "", edadCliente = "") {
+    this.nombre = this.comprobarNombre(nombre);
+    this.correo = this.comprobarCorreo(correo);
+    this.fecha = this.comprobarFecha(fecha);
+    this.hora = this.comprobarHora(hora);
+    this.numNoches = this.comprobarNumNoches(numNoches);
+    this.numPersonas = this.comprobarNumPersonas(numPersonas);
+    this.servicioRes = this.comprobarServicios(servicioRes);
+    this.edadCliente = this.comprobarEdadClientes(edadCliente);
+
+    this.id = this.uniqueID();
+    this.mostrar();
+}
+
+const patrones = {
+
+    nombre: [/^[A-Z][a-z]{1,30}$/, "Primera letra en mayuscula, las siguientes en minusculas."],
+    correo: [/^[a-z]{1,10}[@][a-z]{1,6}[\.][a-z]{1,4}$/, "Formato Esperado: eeeee@eeee.eee donde 'e' es cualquier letra."],
+    fecha: [/^(\d\d)([-\/\: ])(\d\d)\2(\d\d\d\d)$/, "Formato Esperado: dd/mm/yyyy, dd-mm-yyyy o dd mm yyyy  donde 'e' es cualquier letra."],
+    hora: [/^\d\d[\:]\d\d$/, "Formato Esperado: nn:nn donde 'n' es cualquier numero."],
+    numNoches: [/\d+/, "Debe ser un número"],
+    numPersonas: [/\d+/, "Debe de ser un número"],
+    servicioRes: [/[dac]?/i, "Debe de ser un texto formado por 'd' si quieres desayuno,'a' si quieres almuerzo,'c' si quieres cena. Por ejemplo si quieres todo sería: dac"],
+    edadCliente: [/\d+/, "Debe de ser un número"]
+};
+
+Reserva.prototype.patrones = patrones;
+Reserva.prototype.comprobarNombre = function (nombre) {
+    if (!patrones['nombre'][0].test(nombre))
+        throw new Error('Nombre Invalido');
+    return nombre;
+
+};
+
+Reserva.prototype.comprobarCorreo = function (correo) {
+    if (!patrones['correo'][0].test(correo))
+        throw new Error('Correo Invalido');
+    return correo;
+};
+
+Reserva.prototype.comprobarFecha = function (fecha) {
+    [, dia, , mes, anio] = patrones['fecha'][0].exec(fecha);
+    if (parseInt(dia) > 31 || parseInt(mes) > 12) {
+        throw new Error('Fecha invalida');
+    }
+    switch (mes) {
+        case '02':
+            if (parseInt(dia) > 29) {
+                throw new Error('Fecha invalida');
+            }
+            if (parseInt(dia) === 29) {
+                if (!((anio % 4 == 0) && ((anio % 100 != 0)) || (anio % 400 == 0))) {
+                    throw new Error('Fecha invalida');
+                }
+            }
+            break;
+        case '01':
+        case '03':
+        case '05':
+        case '07':
+        case '08':
+        case '10':
+        case '12':
+            if (parseInt(dia) > 31) {
+                throw new Error('Fecha invalida');
+            }
+            break;
+        case '04':
+        case '06':
+        case '09':
+        case '11':
+            if (parseInt(dia) > 30) {
+                throw new Error('Fecha invalida');
+            }
+            break;
+
+    }
+    return fecha;
+};
+
+Reserva.prototype.comprobarHora = function (hora) {
+    if (!patrones['hora'][0].test(hora))
+        throw new Error('Hora invalida');
+    return hora;
+};
+
+Reserva.prototype.comprobarNumNoches = function (numNoches) {
+    if (!patrones['numNoches'][0].test(numNoches))
+        throw new Error('Número de noches invalida');
+    return numNoches;
+};
+
+Reserva.prototype.comprobarNumPersonas = function (numPersonas) {
+    if (!patrones['numPersonas'][0].test(numPersonas))
+        throw new Error('Número de personas invalida');
+    return numPersonas;
+};
+
+Reserva.prototype.comprobarServicios = function (servicios) {
+    let mensaje = "";
+    if (/d/.test(servicios))
+        mensaje += "Desayuno "
+    if (/a/.test(servicios))
+        mensaje += "Almuerzo "
+    if (/c/.test(servicios))
+        mensaje += "Cena "
+    mensaje === "" ? mensaje = "Ningún servicio" : "";
+    return mensaje;
+}
+Reserva.prototype.comprobarEdadClientes = function (edadCliente) {
+    if (edadCliente === '') 
+        throw new Error('Debe seleccionar una edad');
+    return edadCliente;
+}
+
+Reserva.prototype.uniqueID = (function () {
+    let id = 0;
+    return function () {
+        return id++;
+    };
+})();
+
+Reserva.prototype.mostrar = function () {
+
+    ventana = window.open("", "", "height=200,width=300,top=50,left=50");
+    ventana.document.write(
+        `
+    <html>
+    <body>
+        Nombre: ${this.nombre}<br>
+        Correo : ${this.correo}<br>
+        Fecha : ${this.fecha}<br>
+        Hora : ${this.hora}<br>
+        Número de noches: ${this.numNoches}<br>
+        Número de Personas : ${this.numPersonas}<br>
+        Servicios Restaurante : ${this.servicioRes}<br>
+        Edad cliente  ${this.edadCliente}<br>
+        Id de la reserva : ${this.id} <br>
+
+        <p>Reserva Realizada por Marcos Gallardo Pérez</p>
+    </body>
+    </html>
+    `);
+    ventana.document.close();
+}
---
+++ 1.Cliente/Tema5/ExamenJavaScript/js/main.js
@@ -0,0 +1,127 @@
+/**
+ * Comportamiento del html
+ * @author Marcos Gallardo Pérez
+ */
+
+{
+    let reserva
+    document.addEventListener('DOMContentLoaded', init);
+    let inputs;
+    let spans;
+    let domDate;
+    let domEdad;
+
+    function init() {
+        document.getElementById('button').addEventListener('click', comprobarFormulario)
+        inputs = document.getElementsByTagName("input");
+        spans = document.getElementsByTagName('span');
+        domDate = document.getElementById('sfecha');
+        domEdad = document.getElementById('edad');
+        Array.from(inputs).pop();
+        Array.from(spans).pop();
+        for (let element of inputs) {
+            if (compruebaElementosExcentos(element)) {
+                continue;
+            }
+            element.addEventListener('blur', compruebaCampo.bind(null, element));
+        }
+    }
+
+    function compruebaCampo(element) {
+        if (compruebaElementosExcentos(element)) {
+            return;
+        }
+
+        let span;
+        for (let eSpan of spans) {
+            if (("s" + element.id) === eSpan.id)
+                span = eSpan;
+        }
+        if (!patrones[element.id][0].test(element.value)) {
+            span.innerHTML = " " + patrones[element.id][1];
+
+        } else
+            span.innerHTML = "";
+
+    }
+
+    function comprobarFormulario() {
+        let nombre;
+        let correo;
+        let fecha;
+        let hora;
+        let numNoches;
+        let numPersonas;
+        let servicioRes = "";
+        let edadCliente = '';
+        domEdad.style.border = '1px Solid black';
+        for (let element of inputs) {
+            compruebaCampo(element);
+            switch (element.id) {
+                case "nombre":
+                    nombre = element.value;
+                    break;
+                case "correo":
+                    correo = element.value;
+                    break;
+                case "fecha":
+                    fecha = element.value;
+                    break;
+                case "hora":
+                    hora = element.value;
+                    break;
+                case "numNoches":
+                    numNoches = element.value;
+                    break;
+                case "numPersonas":
+                    numPersonas = element.value;
+                case "servRestauranteD":
+                    if (element.checked)
+                        servicioRes += 'd';
+                    break;
+                case "servRestauranteA":
+                    if (element.checked)
+                        servicioRes += 'a';
+                    break;
+                case "servRestauranteC":
+                    if (element.checked)
+                        servicioRes += 'c';
+                    break;
+                case "edad20":
+                    if (element.checked)
+                        edadCliente = 'Menor que 20';
+                    break;
+                case "edad20y40":
+                    if (element.checked)
+                        edadCliente = 'Entre 20 y 40';
+                    break;
+                case "edad40":
+                    if (element.checked)
+                        edadCliente = 'Mayor de 40';
+                    break;
+            }
+        }
+        try {
+            reserva = new Reserva(nombre, correo, fecha, hora, numNoches, numPersonas, servicioRes, edadCliente);
+        } catch (e) {
+            if (e.message === 'Fecha invalida') {
+                domDate.innerHTML = ' Fecha invalida';
+            } else if (e.message === 'Debe seleccionar una edad') {
+                domEdad.style.border = '3px Solid red';
+            }
+            //console.log(e.message);
+            for (let element of spans) {
+                if (element.textContent != "") {
+                    document.getElementById(element.id.replace("s", "")).focus();
+                    return;
+                }
+            }
+        }
+    }
+
+    function compruebaElementosExcentos(element) {
+        return /servRestaurante/.test(element.id) || /edad/.test(element.id) || /button/.test(element.id);
+    }
+
+
+}
