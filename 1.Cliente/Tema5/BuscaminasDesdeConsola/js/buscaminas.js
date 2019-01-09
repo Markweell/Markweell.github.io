d5c001dd3580df4281e1a83532b87d8e28feb7cd
Markweell <marcosgallardoperez@gmail.com>
2019-01-09 14:59:39 +0100

    Añado despejar al Buscaminas desde Consola

--- 1.Cliente/Tema5/BuscaminasDesdeConsola/js/buscaminas.js
+++ 1.Cliente/Tema5/BuscaminasDesdeConsola/js/buscaminas.js
@@ -15,7 +15,8 @@ let buscaminas = (function () {
         console.log("Bienvenido al buscaminas. Para jugar" +
             " primero inicia un tablero con buscaminas.init(), para picar usa" +
             " buscaminas.picar(i,j) donde i y j son las coordenadas de la casilla," +
-            " para poner una bandera usa buscaminas.marcar(i,j)")
+            " para poner una bandera usa buscaminas.marcar(i,j) y para despejar una casilla"+
+            +" usa buscaminas.despejar(i,j) lo que despejara una casilla que tenga banderas cerca");
     }
 
     function init(dificultad = 1) {
@@ -31,7 +32,7 @@ let buscaminas = (function () {
             case 3:
                 numFilas = 30;
                 numComlumnas = 30;
-                numMinas = 99;
+                numMinas = 30;
                 break;
             case 1:
             default:
@@ -112,42 +113,9 @@ let buscaminas = (function () {
         return Math.round(Math.random() * (max - min) + min);
     }
 
-    function mostrarTableroJuego() {
-        tablero = "  ";
-        for (let i = 0; i < campoMinas.length; i++) {
-            if (i == 0) {
-                for (let j = 0; j < campoMinas[i].length; j++) {
-                    if (j < 10) {
-                        tablero += " ";
-                    }
-                    tablero += (j + 1) + " ";
-                }
-                tablero += "\n";
-            }
-            for (let j = -1; j < campoMinas[i].length; j++) {
-                if (j === -1) {
-                    if (i < 9) {
-                        tablero += " ";
-                    }
-                    tablero += (i + 1) + " ";
-                    continue;
-                }
-                if (campoMinas[i][j].tapada == false) {
-                    tablero += campoMinas[i][j].valor + "  ";
-                } else if (campoMinas[i][j].bandera == true) {
-                    tablero += "╦ " + " ";
-                } else
-                    tablero += "■ " + " ";
-            }
-            tablero += "\n";
-        }
-        console.log(tablero);
-    }
-
     function picar(i, j) {
-        if (i < 0 || j >= campoMinas.length) {
-            throw new Error("Fuera de rango!");
-        }
+        comprobarDentroRango(i, j);
+
         if (campoMinas[i][j].bandera) {
             throw new Error("No puedes picar si hay una bandera!");
         }
@@ -185,17 +153,92 @@ let buscaminas = (function () {
                 if (campoMinas[i - 1][j + 1].tapada)
                     picar(i - 1, j + 1);
         }
+
     }
 
-
     function marcar(i, j) {
-        if (i < 0 || j >= campoMinas.length) {
-            throw new Error("Fuera de rango!");
-        }
+        comprobarDentroRango(i, j);
         if (!campoMinas[i][j].tapada) {
             throw new Error("No puedes poner una bandera en una casilla descubierta.");
         }
         campoMinas[i][j].bandera ? campoMinas[i][j].bandera = false : campoMinas[i][j].bandera = true;
+    }
+
+    function despejar(i, j) {
+        let numBanderas = 0;
+        if(campoMinas[i][j].tapada){
+            throw new Error("No puedes despejar una casilla tapada.");
+        }
+
+        if (i != 0)
+            if (campoMinas[i - 1][j].bandera)
+                numBanderas++;
+        if (i != campoMinas.length - 1)
+            if (campoMinas[i + 1][j].bandera)
+                numBanderas++;
+        if (j != campoMinas[1].length - 1)
+            if (campoMinas[i][j + 1].bandera)
+                numBanderas++;
+        if (j != 0)
+            if (campoMinas[i][j - 1].bandera)
+                numBanderas++;
+        if (j !== 0 && i !== campoMinas.length - 1)
+            if (campoMinas[i + 1][j - 1].bandera)
+                numBanderas++;
+        if (i != 0 && j != 0)
+            if (campoMinas[i - 1][j - 1].bandera)
+                numBanderas++;
+        if (i != campoMinas.length - 1 && j != campoMinas[1].length - 1)
+            if (campoMinas[i + 1][j + 1].bandera)
+                numBanderas++;
+        if (i != 0 && j != campoMinas[1].length - 1)
+            if (campoMinas[i - 1][j + 1].bandera)
+                numBanderas++;
+
+        if (numBanderas == campoMinas[i][j].valor) {
+            if (i != 0)
+                if (campoMinas[i - 1][j].tapada && !campoMinas[i - 1][j].bandera)
+                    picar(i - 1, j);
+            if (i != campoMinas.length - 1)
+                if (campoMinas[i + 1][j].tapada && !campoMinas[i + 1][j].bandera)
+                    picar(i + 1, j);
+            if (j != campoMinas[1].length - 1)
+                if (campoMinas[i][j + 1].tapada && !campoMinas[i][j + 1].bandera)
+                    picar(i, j + 1);
+            if (j != 0)
+                if (campoMinas[i][j - 1].tapada && !campoMinas[i][j - 1].bandera)
+                    picar(i, j - 1);
+            if (j !== 0 && i !== campoMinas.length - 1)
+                if (campoMinas[i + 1][j - 1].tapada && !campoMinas[i + 1][j - 1].bandera)
+                    picar(i + 1, j - 1);
+            if (i != 0 && j != 0)
+                if (campoMinas[i - 1][j - 1].tapada && !campoMinas[i - 1][j - 1].bandera)
+                    picar(i - 1, j - 1);
+            if (i != campoMinas.length - 1 && j != campoMinas[1].length - 1)
+                if (campoMinas[i + 1][j + 1].tapada && !campoMinas[i + 1][j + 1].bandera)
+                    picar(i + 1, j + 1);
+            if (i != 0 && j != campoMinas[1].length - 1)
+                if (campoMinas[i - 1][j + 1].tapada && !campoMinas[i - 1][j + 1].bandera)
+                    picar(i - 1, j + 1);
+        }
+    }
+
+    function comprobarDentroRango(i, j) {
+        if (i < 0 || j >= campoMinas.length) {
+            throw new Error("Fuera de rango!");
+        }
+    }
+
+    function picarCasilla(i, j) {
+        accionMostrando(i, j, picar);
+    }
+
+    function marcarCasilla(i, j) {
+        accionMostrando(i, j, marcar);
+    }
+
+    function despejarCasilla(i, j) {
+        accionMostrando(i, j, despejar);
     }
 
     function accionMostrando(i, j, funcion) {
@@ -218,17 +261,47 @@ let buscaminas = (function () {
 
     }
 
-    function picarCasilla(i, j) {
-        accionMostrando(i, j, picar);
+    function mostrarTableroJuego() {
+        tablero = "  ";
+        for (let i = 0; i < campoMinas.length; i++) {
+            if (i == 0) {
+                for (let j = 0; j < campoMinas[i].length; j++) {
+                    if (j < 10) {
+                        tablero += " ";
+                    }
+                    tablero += (j + 1) + " ";
+                }
+                tablero += "\n";
+            }
+            for (let j = -1; j < campoMinas[i].length; j++) {
+                if (j === -1) {
+                    if (i < 9) {
+                        tablero += " ";
+                    }
+                    tablero += (i + 1) + " ";
+                    continue;
+                }
+                pintarResultado(i, j);
+            }
+            tablero += "\n";
+        }
+        console.log(tablero);
+
+        function pintarResultado(i, j) {
+            if (campoMinas[i][j].tapada == false) {
+                tablero += campoMinas[i][j].valor + "  ";
+            } else if (campoMinas[i][j].bandera == true) {
+                tablero += "╦ " + " ";
+            } else
+                tablero += "■ " + " ";
+        }
     }
 
-    function marcarCasilla(i, j) {
-        accionMostrando(i, j, marcar);
-    }
     return {
         init: init,
         mostrar: mostrarTableroJuego,
         picar: picarCasilla,
-        marcar: marcarCasilla
+        marcar: marcarCasilla,
+        despejar: despejarCasilla
     }
 })();
