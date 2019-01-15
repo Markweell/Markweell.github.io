7fe57abb1a37993ea9252759fe228979f1d135bd
Markweell <marcosgallardoperez@gmail.com>
2019-01-15 10:42:45 +0100

    Actualizacion manualJQuery II

--- 1.Cliente/Tema7/practicasManualJQueryIIII,IV,V/js/ejemplo10.js
+++ 1.Cliente/Tema7/practicasManualJQueryIIII,IV,V/js/ejemplo10.js
@@ -2,22 +2,25 @@
  * @author Marcos Gallardo Pérez
  */
 function init() {
-    $(".verde").live("click", function (e) {
+    function cambiaTexto() {
         var elem = $(this);
         if (elem.html() != "Hiciste clic!!") {
             elem.html("Hiciste clic!!");
         } else {
             elem.html("Hiciste de nuevo clic!!");
         }
-    })
+    }
+    $(".verde").on("click", cambiaTexto);
 
     $("#insertarelem").click(function (e) {
         var nuevoElemento = $('<div class="verde">Este elemento se ha creado e insertado dinamicamente (haz clic)</div>');
         nuevoElemento.appendTo($(document.body));
+        $(".verde").on("click", cambiaTexto);
     });
 
     $("#ponerclaseverde").click(function (e) {
         $("#noverde").addClass("verde");
+        $(".verde").on("click", cambiaTexto);
     });
 }
 $(init);
