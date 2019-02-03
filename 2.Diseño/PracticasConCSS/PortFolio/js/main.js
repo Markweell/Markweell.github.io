d8aa475e08aab635bac351647e45e0d531696f46
Markweell <marcosgallardoperez@gmail.com>
2019-02-03 15:13:07 +0100

    Arreglando problemas con algunas resoluciones

--- 2.Diseño/PracticasConCSS/PortFolio/js/main.js
+++ 2.Diseño/PracticasConCSS/PortFolio/js/main.js
@@ -43,21 +43,22 @@
             subirElemento(cuerpoAndroid);
             posicion = 5;
         })
-        $("#buscaminas").click(()=>{
-            location.href="https://github.com/Markweell/Markweell.github.io/tree/master/1.Cliente/Tema7/BuscaminasConJQuery";
+        $("#buscaminas").click(() => {
+            location.href = "https://github.com/Markweell/Markweell.github.io/tree/master/1.Cliente/Tema7/BuscaminasConJQuery";
         })
-        $("#casasRurales").click(()=>{
-            location.href="https://github.com/Markweell/Markweell.github.io/tree/master/2.Dise%C3%B1o/PracticasConCSS/CasasRurales";
+        $("#casasRurales").click(() => {
+            location.href = "https://github.com/Markweell/Markweell.github.io/tree/master/2.Dise%C3%B1o/PracticasConCSS/CasasRurales";
         })
-        $("#JuegoJava").click(()=>{
-            location.href="https://github.com/Markweell/ProyectoJuegoJava/tree/master/proyecto_final_2018";
+        $("#JuegoJava").click(() => {
+            location.href = "https://github.com/Markweell/ProyectoJuegoJava/tree/master/proyecto_final_2018";
         })
         $("#container").mouseover((ev) => {
             x = ev.offsetX / 80
             y = ev.offsetY / 80
-            fondoPantallaDom.css({
-                "background-position": (x * 4 - 100) + "px " + (y * 4 - 100) + "px"
-            })
+            if (screen.width > 1270)
+                fondoPantallaDom.css({
+                    "background-position": (x * 4 - 100) + "px " + (y * 4 - 100) + "px"
+                })
         })
         $(window).keydown((ev) => {
             if (ev.key === "ArrowRight") {
