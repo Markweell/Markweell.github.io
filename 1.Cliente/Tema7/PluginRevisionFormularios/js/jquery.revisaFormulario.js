56b36f5407f5a08727b15b0dab0959710b6fb548
Markweell <marcosgallardoperez@gmail.com>
2019-02-03 21:07:02 +0100

    Versión alpha Plugin Revisión Formulario

--- 1.Cliente/Tema7/PluginRevisionFormularios/js/jquery.revisaFormulario.js
+++ 1.Cliente/Tema7/PluginRevisionFormularios/js/jquery.revisaFormulario.js
@@ -1,3 +1,28 @@
-jQuery.fn.revisaFormulario = function (input,patron){
-
+const patrones = {
+    nombre: /^[a-zA-Zñúíóáé]{3,}([ ][a-zA-Zñúíóáé]{3,}){0,10}$/,
+    apellidos: /^[a-zA-Zñúíóáé]{3,}([ ][a-zA-Zñúíóáé]{3,}){0,10}$/,
+    correo: /^[a-zA-Zñ]{1,10}([.][a-zA-Zñ]{1,10}){0,3}[@][a-z]{1,6}([\.][a-z]{1,4}){1,4}$/
+}
+jQuery.fn.revisaFormulario = function () {
+    focusedo=false;
+    this.each(function () {
+        if (!patrones[$(this).attr("tipo")].test($(this).val())){
+            $(this).css({
+                'border': '2px solid #ffD3D7',
+                'background': '#ffDEDE',
+                'color': '#ff0000'
+            })
+            if(!focusedo){
+                $(this).focus();
+                focusedo=true;
+            }
+        }
+        else
+            $(this).css({
+                'border': '1px solid black',
+                'background': 'white',
+                'color': 'black'
+            })
+    });
+    return $(this);
 }
--- 1.Cliente/Tema7/PluginRevisionFormularios/js/main.js
+++ 1.Cliente/Tema7/PluginRevisionFormularios/js/main.js
@@ -2,9 +2,59 @@
     /**
      * @author Marcos Gallardo Pérez
      */
-    $(init)
+
+    let inputs,
+        textArea;
 
     function init() {
+        inputs = $('input[type="text"]');
+        textArea = $('textarea');
+        textArea.prop("disabled", true);
+        
+        $('input[type="submit"]').click((e) => {
+            e.preventDefault();
+            let correctos = true;
+            let mensaje = "";
+            inputs.revisaFormulario();
+            inputs.each(function (indice, elemento) {
+                elemento = $(elemento);
+                mensaje += elemento.attr("tipo") + ": " + elemento.val() + "\n";
+                if (elemento.css('background-color') != 'rgb(255, 255, 255)' || elemento.val()=="")
+                    correctos=false  
+            });
+            correctos?textArea.text(mensaje):textArea.text(""); 
+        });
 
+        inputs.blur((e) => {
+            $(e.delegateTarget).revisaFormulario();
+        });
+        inputs.focus((e) => {
+            $(e.delegateTarget).css({
+                'transition':'0.1s',
+                'transition-delay':'0.1s',
+                'border': '1px solid black',
+                'background': 'white',
+                'color': 'black'
+            });
+        });
+        console.log(tester.init('nombre', 'nombre'))
     }
+    $(init);
+    /**
+     * Clousure
+     */
+    let tester = (function () {
+        let mensaje;
+
+        function init(texto, tipo) {
+            mensaje = "Incorrecto";
+            if (patrones[tipo].test(texto)) {
+                mensaje = "Correcto"
+            }
+            return mensaje;
+        }
+        return {
+            init
+        }
+    })();
 }
