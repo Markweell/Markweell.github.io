5c1f7310f822c344a89786bb2c9d6a42579572ba
Markweell <marcosgallardoperez@gmail.com>
2019-02-17 11:29:26 +0100

    Ajax y json

--- 1.Cliente/Tema7/ClasificacionesJSON/js/main.js
+++ 1.Cliente/Tema7/ClasificacionesJSON/js/main.js
@@ -5,8 +5,29 @@
 
 
     function init() {
+       $('input[type=button]').click(function clickButton(){
         
+        let divContenedor = $('<div></div>');
+        $.getJSON("json/"+$(this).attr("name")+".json",function callback(resultado){
+            let i = 0;
+            $.each(resultado, (key,value)=>{
+                divContenedor.append("<div class='container' id='container_"+i+"'>"+
+                "<h1 id='titulo_"+i+"'><span >"+key+"</span></h1>"+
+                "<p id='parrafo_"+i+"'>"+value+"</p></div>");
+                i++;
+            })
+            $('.container').click(function muestraContenido(){
+                separador = this.id.split("_");
+                if($("#parrafo_"+separador[1]).css("display")==="none")
+                    $("#parrafo_"+separador[1]).css({"display":"inline"});
+                else
+                    $("#parrafo_"+separador[1]).css({"display":"none"});
+               
+            })
+        })
+        $('article').html(divContenedor);
+       }) 
     }
     $(init);
- 
 }
+    
