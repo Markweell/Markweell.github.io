0c3a9c329a582fd9714c011c853e5096d168f6d6
Markweell <marcosgallardoperez@gmail.com>
2019-02-25 12:44:35 +0100

    cambios menores

--- 1.Cliente/Tema7/SugerenciasMediantePhp/js/main.js
+++ 1.Cliente/Tema7/SugerenciasMediantePhp/js/main.js
@@ -9,26 +9,53 @@
         inputDom = $('input[type=text]');
         let mensaje = "";
         inputDom.keyup(() => {
-            mensaje = "";
-            $.get('php/codigoPHP.php', {
+            $.ajax({
+                url: 'php/codigoPHP.php',
+                data: {
                     'textoInput': inputDom.val()
                 },
-                (result) => {
+                success: function success(result) {
+                    $('#sugerencia').html("");
+                    mensaje = "";
                     $.each(JSON.parse(result), function (key, value) {
-                        if (value === "No hay coincidencias" || inputDom.val()==="") {
+                        if (value === "No hay coincidencias" || inputDom.val() === "") {
                             $('#sugerencia').html("<div>No hay ninguna coincidencia</div>");
                             return;
                         }
                         mensaje += "<li class='elementos'>" + value + "</li>";
                         $('#sugerencia').html(mensaje);
-                        $('.elementos').click(function asignarElementoEncontrado() {
-                            inputDom.val(this.innerText);
-                            inputDom.keyup();
-                        })
+
                     });
-                }).fail(function () {
-                $('#sugerencia').text("La conexión por get con el servidor no se pudo realizar.");
-            })
+                },
+                dataType: "text",
+                complete: () => {
+                    $('#sugerencia').html(mensaje);
+                    $('.elementos').click(function asignarElementoEncontrado() {
+                        inputDom.val(this.innerText);
+                        inputDom.keyup();
+                    })
+                }
+            });
+            // $.get('php/codigoPHP.php', {
+            //         'textoInput': inputDom.val()
+            //     },
+            //     (result) => {
+            //         $.each(JSON.parse(result), function (key, value) {
+            //             if (value === "No hay coincidencias" || inputDom.val() === "") {
+            //                 $('#sugerencia').html("<div>No hay ninguna coincidencia</div>");
+            //                 return;
+            //             }
+            //             console.log(mensaje)
+            //             mensaje += "<li class='elementos'>" + value + "</li>";
+            //             $('#sugerencia').html(mensaje);
+            //             $('.elementos').click(function asignarElementoEncontrado() {
+            //                 inputDom.val(this.innerText);
+            //                 inputDom.keyup();
+            //             })
+            //         });
+            //     }).fail(function () {
+            //     $('#sugerencia').text("La conexión por get con el servidor no se pudo realizar.");
+            // })
         })
     }
     $(init);
