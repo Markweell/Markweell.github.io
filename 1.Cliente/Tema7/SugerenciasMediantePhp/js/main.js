3d3a5c0e2b710d89df895a57b8665a1dd0415041
Markweell <marcosgallardoperez@gmail.com>
2019-02-25 11:01:56 +0100

    arreglando archivos

--- 1.Cliente/Tema7/SugerenciasMediantePhp/js/main.js
+++ 1.Cliente/Tema7/SugerenciasMediantePhp/js/main.js
@@ -6,28 +6,31 @@
     let inputDom;
 
     function init() {
-        inputDom=$('input[type=text]');
-        let mensaje="";
-        inputDom.keyup(()=>{
-            mensaje="";
-            $.get('php/servidor.php',{'textoInput': inputDom.val()},(result)=>{
-                $.each(result.split(","),(key,value)=>{
-                    if(value==="No hay ninguna coincidencia"){
-                        mensaje +="<div>"+value+"</div>";
-                        return;
-                    }else if(value!="")
-                        mensaje += "<li class='elementos'>"+value+"</li>";
-                })
-                $('#sugerencia').html(mensaje);
-                $('.elementos').click(function asignarElementoEncontrado(){
-                    inputDom.val(this.innerText);
-                    inputDom.keyup();
-                })
-            }).fail(function () {
+        inputDom = $('input[type=text]');
+        let mensaje = "";
+        inputDom.keyup(() => {
+            mensaje = "";
+            $.get('php/codigoPHP.php', {
+                    'textoInput': inputDom.val()
+                },
+                (result) => {
+                    $.each(JSON.parse(result), function (key, value) {
+                        if (value === "No hay coincidencias" || inputDom.val()==="") {
+                            $('#sugerencia').html("<div>No hay ninguna coincidencia</div>");
+                            return;
+                        }
+                        mensaje += "<li class='elementos'>" + value + "</li>";
+                        $('#sugerencia').html(mensaje);
+                        $('.elementos').click(function asignarElementoEncontrado() {
+                            inputDom.val(this.innerText);
+                            inputDom.keyup();
+                        })
+                    });
+                }).fail(function () {
                 $('#sugerencia').text("La conexión por get con el servidor no se pudo realizar.");
-            })   
+            })
         })
     }
     $(init);
- 
+
 }
--- 1.Cliente/Tema7/SugerenciasMediantePhp/php/servidor.php
+++ 1.Cliente/Tema7/SugerenciasMediantePhp/php/servidor.php
@@ -1,25 +1,25 @@
 <?php
-$companieros = ['Angelo Barbara',
-'Francisco', 'Guillermo Boquizo',
- 'Suso Mejías', 'Er zuri', 'Bravo',
- 'Jose Rafael', 'Mario', 'Samuel',
- 'Javi', 'Sojo', 'Chema','Marcos'];
+$companieros = ['nombre'=>'Angelo Barbara',
+'nombre2'=>'Francisco Ramirez Ruiz', 'nombre3'=>'Guillermo Boquizo ',
+ 'Suso Mejías', 'Rafael Zurita', 'Jose Manuel Bravo',
+ 'Jose Rafael Alvarez', 'Mario Navarro', 'Samuel',
+ 'Javier ', 'Rafael Sojo', 'Chema','Marcos Gallardo Pérez'];
+json_encode($companieros, JSON_FORCE_OBJECT);
+// if (isset($_GET['textoInput'])) {
+//     $mensaje = "";
+//     if ($_GET['textoInput']==="") {
+//         echo "No hay ninguna coincidencia";
+//         return;
+//     }
+//     foreach ($companieros as $companiero) {
+//         if (stristr($companiero,$_GET['textoInput'])) {
+//             $mensaje .= $companiero.",";
+//         }
 
-if (isset($_GET['textoInput'])) {
-    $mensaje = "";
-    if ($_GET['textoInput']==="") {
-        echo "No hay ninguna coincidencia";
-        return;
-    }
-    foreach ($companieros as $companiero) {
-        if (stristr($companiero,$_GET['textoInput'])) {
-            $mensaje .= $companiero.",";
-        }
-
-    }
-    if ($mensaje === "") {
-        $mensaje = "No hay ninguna coincidencia";
-    }
-    echo $mensaje;
-}
+//     }
+//     if ($mensaje === "") {
+//         $mensaje = "No hay ninguna coincidencia";
+//     }
+    
+// }
 ?>
